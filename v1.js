15ff18ee366d6a99033a32786192b18c4f2aa8f1
Bjørn <{ID}+{username}@users.noreply.github.com>
2020-02-15 22:04:08 +0100

    API version 1

---
+++ v1.js
@@ -0,0 +1,16 @@
+const tilestacker = require("./src/tilestacker");
+const config = require("./data/config");
+
+const v1 = async (frag, res) => {
+  if (frag[0] === "layers") return config;
+  if (frag.length === 4) {
+    const node = await tilestacker.get(config, ...frag);
+    if (!node) return "No node!!";
+    if (!node.contentType) return "no content-type";
+    res.setHeader("Content-Type", node.contentType);
+    return node.buffer;
+  }
+  return frag;
+};
+
+module.exports = v1;
