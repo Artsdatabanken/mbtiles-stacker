76be2995394961cabdbb27047ef69bb0d9fdf85c
Bjørn <{ID}+{username}@users.noreply.github.com>
2020-02-15 22:42:41 +0100

    Add CORS

--- index.js
+++ index.js
@@ -12,29 +12,31 @@ process.on("unhandledRejection", (a, b, c) => {
 
 const redirect = (location, res) => {
   res.setHeader("Location", location);
-  send(res, 301, "");
+  send(res, 301);
 };
 
 module.exports = async (req, res) => {
-  try {
-    res.setHeader("X-Powered-By", "mbtiles-stacker v" + pjson.version);
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET");
-    res.setHeader("Access-Control-Expose-Headers", "Content-Length");
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Accept, Authorization, Content-Type, X-Requested-With"
-    );
-    console.log(req.method);
-    if (req.method === "OPTIONS") return res.send(200);
+  res.setHeader("X-Powered-By", "mbtiles-stacker v" + pjson.version);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  res.setHeader("Access-Control-Expose-Headers", "Content-Length");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Accept, Authorization, Content-Type, X-Requested-With"
+  );
+  if (req.method === "OPTIONS") return send(res, 200);
 
+  const r = await doit(req, res);
+  return r;
+};
+const doit = async (req, res) => {
+  try {
     const parts = decodeURIComponent(req.url)
       .split(/\//g)
       .filter(x => x.length > 0);
     const [version, ...rest] = parts;
-    console.log(version, rest);
-    if (version === "swagger") return swagger(parts[1], res);
+    if (version === "swagger") return await swagger(parts[1], req, res);
 
     if (version === "v1")
       return await v1(rest, res).catch(e => sendError(req, res, e));
--- src/swagger.js
+++ src/swagger.js
@@ -1,3 +1,4 @@
+const { sendError } = require("micro");
 const path = require("path");
 const fs = require("fs").promises;
 
@@ -9,11 +10,15 @@ const mimeTypes = {
   css: "text/css"
 };
 
-const swagger = async (file = "index.html", res) => {
+const swagger = async (file = "index.html", req, res) => {
   const basePath = path.resolve("./swagger");
   const fpath = path.join(basePath, file);
-  const buff = await fs.readFile(fpath);
-
+  var err = null;
+  const buff = await fs.readFile(fpath).catch(e => {
+    err = new Error();
+    err.statusCode = 404;
+  });
+  if (err) return sendError(req, res, err);
   const ext = file.split(".").pop();
   res.setHeader("Content-Type", mimeTypes[ext] || "text/html");
   return buff;
